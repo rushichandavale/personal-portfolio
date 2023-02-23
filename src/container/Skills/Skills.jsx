c67ddedba7325a4b6b233092cde3a2964a6a8b10
Rushikesh Chandavale <rushichandavale@gmail.com>
2023-02-24 00:34:26 +0530

    initial commit

---
+++ src/container/Skills/Skills.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+// import RaectTooltip from "react-tooltip";
+import { motion } from "framer-motion";
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { images } from "../../constants";
+import "./Skills.scss";
+const Skills = () => {
+  const skills = [
+    {
+      name: "HTML",
+      icon: images.html,
+      bgColor: "#edf2f8",
+    },
+    {
+      name: "CSS",
+      icon: images.css,
+      bgColor: "#edf2f8",
+    },
+    {
+      name: "Sass",
+      icon: images.sass,
+      bgColor: "#edf2f8",
+    },
+    {
+      name: "Tailwind CSS",
+      icon: images.tailwind,
+      bgColor: "#edf2f8",
+    },
+    {
+      name: "Bootstrap",
+      icon: images.bootstrap,
+      bgColor: "#edf2f8",
+    },
+    {
+      name: "JavaScript",
+      icon: images.javascript,
+      bgColor: "#edf2f8",
+    },
+    {
+      name: "Reactjs",
+      icon: images.react,
+      bgColor: "#edf2f8",
+    },
+    {
+      name: "Nodejs",
+      icon: images.node,
+      bgColor: "#edf2f8",
+    },
+    {
+      name: "Expressjs",
+      icon: images.expressjs,
+      bgColor: "#edf2f8",
+    },
+    {
+      name: "MongoDB",
+      icon: images.mongodb,
+      bgColor: "#edf2f8",
+    },
+    
+    {
+      name: "Redux",
+      icon: images.redux,
+      bgColor: "#edf2f8",
+    },
+    
+    {
+      name: "Firebase",
+      icon: images.firebase,
+      bgColor: "#edf2f8",
+    },
+  ];
+  return (
+    <>
+      <h2 className="head-text">Skills</h2>
+      <div className="app__skills-container">
+        <motion.div className="app__skills-list">
+          {skills.map((skill) => (
+            <motion.div
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skills-item app__flex"
+              key={skill.name}
+            >
+              <div
+                className="app__flex"
+                style={{ backgroundColor: skill.bgColor }}
+              >
+                <img src={skill.icon} alt="skills" />
+              </div>
+              <p className="p-text">{skill.name}</p>
+            </motion.div>
+          ))}
+        </motion.div>
+      </div>
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(Skills, "app__skills"),
+  "skills",
+  "app__whitebg"
+)
---
+++ src/container/Work/WorkData.js
@@ -0,0 +1,47 @@
+import { images } from "../../constants";
+const works = [
+   {
+      title: " Snake Game",
+      description: "A simple Snake game using HTML CSS & JavaScript",
+      codeLink:"https://github.com/rushichandavale/snake-game",
+      projectLink: "https://heartan-snake-game.netlify.app/",
+      imageUrl: images.snakegame,
+      
+   },
+   {
+      title: " Youtube Clone",
+      description: "A React App with a modern UI",
+      codeLink:"https://github.com/rushichandavale/youtube_clone_react",
+      projectLink: "https://heartan-ytc.netlify.app/",
+      imageUrl: images.ytc,
+      
+   },
+   {
+      title: " Restaurant Website",
+      description: "A React App with a modern UI",
+      codeLink:"https://github.com/rushichandavale/heartan-restaurant",
+      projectLink: "https://heartan-restaurant.netlify.app/",
+      imageUrl: images.restaurant,
+      
+   },
+   
+   {
+      title: " E-Commerce Website",
+      description: "E-Commerce Wbsite using HTML CSS & JavaScript",
+      codeLink:"https://github.com/rushichandavale/e_commerce-website",
+      projectLink: "https://heartan-shopping.netlify.app/",
+      imageUrl: images.ecommerce,
+      
+   },
+   {
+      title: "React Calculator",
+      description: "A React App with a modern UI",
+      codeLink:"https://github.com/rushichandavale/react-calculator",
+      projectLink: "https://heartan-react-calculator.netlify.app/",
+      imageUrl: images.calculator,
+      
+   },
+   
+];
+
+export default works;
